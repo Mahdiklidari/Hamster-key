9f7131ad44d7759b5ca815d399b1d682c579d4b6
MAHDI__MK__klidari <92400505+Mahdiklidari@users.noreply.github.com>
2024-08-21 14:08:20 +0330

    Update script.js

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         7 : {
         name: 'Polysphere',
-        appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         eventsDelay: 30000, // 30 seconds
         attempts: 20,
          }
